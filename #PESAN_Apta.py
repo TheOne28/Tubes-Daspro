77abf750ea494b7221b34388e86105c0ef5dd80a
Adiyatma <73101777+masakanpadang@users.noreply.github.com>
2021-04-30 00:21:57 +0700

    Create #PESAN_Apta.py

---
+++ #PESAN_Apta.py
@@ -0,0 +1,8 @@
+print("Kamu semangat yaa ngerjain tubesnyaa!!!! kwkwkw")
+print("")
+x = string(input("habis aku semangatin, kamu semangat gakk? (Y/N)"))
+
+if x == "Y":
+  print("MANTEUPPP!!!")
+elif x == "N":
+  print("LAH kenapaa nggaa? :( ada masalah apa? sini ceritaa :)")
