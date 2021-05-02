97430010bce2d383b8c33750b8bdf2b07e23a563
Adiyatma <73101777+masakanpadang@users.noreply.github.com>
2021-05-02 11:36:43 +0700

    Delete #PESAN_Apta.py

--- #PESAN_Apta.py
+++
@@ -1,8 +0,0 @@
-print("Kamu semangat yaa ngerjain tubesnyaa!!!! xixixi")
-print("")
-x = string(input("habis aku semangatin, kamu semangat gakk? (Y/N)"))
-
-if x == "Y":
-  print("MANTEUPPP!!!")
-elif x == "N":
-  print("LAH kenapaa nggaa? :( ada masalah apa? sini ceritaa :)")
